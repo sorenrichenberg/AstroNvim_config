32d582b1d38cbdf8cbf35e8137718b76a1abbdd8
Soren Richenberg <sorensrichenberg@gmail.com>
2024-01-16 15:53:55 -0800

    this whole formatting plugin thing is conflicting

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "gruvbox",
+  colorscheme = "astromars",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
