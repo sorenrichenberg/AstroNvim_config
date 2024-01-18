0e16ced3a5b5074696dd0802cf638bb9c248addb
Soren Richenberg <sorensrichenberg@gmail.com>
2024-01-17 22:39:33 -0800

    test this wat

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,36 @@ return {
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
-  { import = "astrocommunity.colorscheme" }, -- load all community colorschemes (:
+  { import = "astrocommunity.bars-and-lines/heirline-mode-text-statusline" }, -- statusline mode text
+  { import = "astrocommunity.bars-and-lines/heirline-vscode-winbar" },
+  { import = "astrocommunity.register/nvim-neoclip-lua" },
+
+  { import = "astrocommunity.colorscheme" },
+  -- fuck this....
+  -- { import = "astrocommunity.colorscheme/catppuccin" },
+  -- { import = "astrocommunity.colorscheme/dracula-nvim" },
+  -- { import = "astrocommunity.colorscheme/everforest" },
+  -- { import = "astrocommunity.colorscheme/fluormachine-nvim" },
+  -- { import = "astrocommunity.colorscheme/github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme/gruvbox-baby" },
+  -- { import = "astrocommunity.colorscheme/gruvbox-nvim" },
+  -- { import = "astrocommunity.colorscheme/helix-nvim" },
+  -- { import = "astrocommunity.colorscheme/iceberg-vim" },
+  -- { import = "astrocommunity.colorscheme/kanagawa-nvim" },
+  -- { import = "astrocommunity.colorscheme/melange-nvim" },
+  -- { import = "astrocommunity.colorscheme/mellow-nvim" },
+  -- { import = "astrocommunity.colorscheme/mini-base16" },
+  -- { import = "astrocommunity.colorscheme/monokai-pro-nvim" },
+  -- { import = "astrocommunity.colorscheme/neosolarized-nvim" },
+  -- { import = "astrocommunity.colorscheme/nightfox-nvim" },
+  -- { import = "astrocommunity.colorscheme/nord-nvim" },
+  -- { import = "astrocommunity.colorscheme/nordic-nvim" },
+  -- { import = "astrocommunity.colorscheme/nvim-juliana" },
+  -- { import = "astrocommunity.colorscheme/onedarkpro-nvim" },
+  -- { import = "astrocommunity.colorscheme/oxocarbon-nvim" },
+  -- { import = "astrocommunity.colorscheme/poimandres-nvim" },
+  -- { import = "astrocommunity.colorscheme/rose-pine" },
+  -- { import = "astrocommunity.colorscheme/sonokai" },
+  -- { import = "astrocommunity.colorscheme/tokyonight-nvim" },
+  -- { import = "astrocommunity.colorscheme/vscode-nvim" },
 }
